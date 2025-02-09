3f1716805cec3ea78910bec7335d021b1eaf5ab7
Smoctar_L <lomoctar1234@gmail.com>
2025-02-09 18:46:20 -0500

    Page d'intallation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { FavoritesProvider } from './contexts/FavoritesContext';
 import Home from './pages/Home';
 import Quran from './pages/Quran';
 import SurahDetail from './pages/SurahDetail';
 import Favorites from './pages/Favorites';
-import { FavoritesProvider } from './contexts/FavoritesContext';
+import InstallGuide from './pages/InstallGuide';
 
 function App() {
   return (
     <FavoritesProvider>
       <Router>
-        <div className="min-h-screen bg-black text-white">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/quran" element={<Quran />} />
-            <Route path="/quran/:id" element={<SurahDetail />} />
-            <Route path="/favorites" element={<Favorites />} />
-          </Routes>
+        <div className="min-h-screen bg-[#121212]">
+          <div className="relative z-10">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/quran" element={<Quran />} />
+              <Route path="/quran/:id" element={<SurahDetail />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/install" element={<InstallGuide />} />
+            </Routes>
+          </div>
         </div>
       </Router>
     </FavoritesProvider>
