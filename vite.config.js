7dfc59b50bab972a095020b6780d8936a5a54f89
Smoctar_L <lomoctar1234@gmail.com>
2025-02-09 22:51:53 -0500

    Solving download problems :)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Hidaya',
+        short_name: 'Hidaya',
+        display: 'standalone',
+        background_color: '#070907',
+        theme_color: '#070907',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     outDir: 'dist'
   }
