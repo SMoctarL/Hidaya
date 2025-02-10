4e76df2611538a7bc81ff0d8128e9d2636da85a0
Smoctar_L <lomoctar1234@gmail.com>
2025-02-09 22:11:42 -0500

    Solving download problems :)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: true
+  },
+  server: {
+    port: 3000
+  },
+  // Configuration spécifique pour Vercel
+  base: './',
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
 }) 
