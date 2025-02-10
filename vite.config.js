50199bc7460ee085ec2c4c5c4364783a032a0c2e
Smoctar_L <lomoctar1234@gmail.com>
2025-02-09 22:19:52 -0500

    Solving download problems :)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'logo192.png', 'logo512.png'],
+      manifest: {
+        name: 'Hidaya',
+        short_name: 'Hidaya',
+        description: 'Guide de Prière et du Coran',
+        theme_color: '#070907',
+        background_color: '#070907',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     outDir: 'dist',
     sourcemap: true
